a8e4bcdcf4b6ceafab2d91e2dfbb7bbe6e0ae597
marcuscharles-code <charlesfatunsin@gmail.com>
2024-02-28 17:22:51 +0100

    line and bar chart completed

--- client/src/Component/Admin/Charts/BarChart.jsx
+++ client/src/Component/Admin/Charts/BarChart.jsx
@@ -26,17 +26,78 @@ const BarChart = () => {
       },
     ],
   };
-  const div ={
-    maintainAspectRatio: false,
-}
 
-  // Return the Bar component, passing in the data object as a prop.
+  // Define the chart options object.
+  const options = {
+    maintainAspectRatio: false,
+    responsive: true,
+    scales: {
+      x: {
+        display: true,
+      },
+      y: {
+        display: true,
+        beginAtZero: true,
+      },
+    },
+  };
+
+  // Return the Bar component, passing in the data and options objects as props.
   return (
-    <div style={{width: '13rem', height: '13'}}>
-      <Bar data={data} div={div}/>
+    <div className="card">
+      <Bar data={data} options={options} />
     </div>
   );
 };
 
 // Export the BarChart component as the default export of the module.
 export default BarChart;
+
+
+
+
+
+
+
+// // Import the React library.
+// import React from "react";
+
+// // Import the Chart.js library.
+// import Chart from "chart.js/auto";
+
+// // Import the Bar component from the react-chartjs-2 library.
+// import { Bar } from "react-chartjs-2";
+
+// /**
+//  * Define a functional component named BarChart
+//  */
+// const BarChart = () => {
+//   // Define an array of labels.
+//   const labels = ["January", "February", "March", "April", "May", "June"];
+
+//   // Defined an object 
+//   const data = {
+//     labels: labels,
+//     datasets: [
+//       {
+//         label: "My First dataset",
+//         backgroundColor: "rgb(255, 99, 132)",
+//         borderColor: "rgb(255, 99, 132)",
+//         data: [0, 10, 5, 2, 20, 30, 45],
+//       },
+//     ],
+//   };
+//   const div ={
+//     maintainAspectRatio: false,
+// }
+
+//   // Return the Bar component, passing in the data object as a prop.
+//   return (
+//     <div style={{width: '13rem', height: '13'}}>
+//       <Bar data={data} div={div}/>
+//     </div>
+//   );
+// };
+
+// // Export the BarChart component as the default export of the module.
+// export default BarChart;
