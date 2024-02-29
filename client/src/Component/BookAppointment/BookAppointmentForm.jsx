54c6692fa5dc1189cf11b8d2fdf7a835912ddee6
marcuscharles-code <charlesfatunsin@gmail.com>
2024-02-29 18:59:07 +0100

    button added to the appointment page

--- client/src/Component/BookAppointment/BookAppointmentForm.jsx
+++ client/src/Component/BookAppointment/BookAppointmentForm.jsx
@@ -1,35 +1,42 @@
 import React from 'react'
 import './Book.css'
+import { Doctor } from '../img/image'
 
 
 function BookAppointmentForm() {
-  return (
-    <div className='body'>
-      <div className='head'><h2>Book Apointment</h2></div>
-      <form>
-        <div class="input-field">
-            <input type="text" id="name" required />
-            <label for="name">Your name:</label>
+    return (
+
+        <div className='general'>
+            <div className='photo'><img src={Doctor} alt="" /></div>
+            <div className='body'>
+                <div className='head'><h2>Book Apointment</h2></div>
+                <form>
+                    <div class="input-field">
+                        <input type="text" id="name" required />
+                        <label for="name">Your name:</label>
+                    </div>
+                    <div class="input-field">
+                        <input type="text" id="Gender" required />
+                        <label for="Gender">Specialist:</label>
+                    </div>
+                    <div class="input-date">
+                        <input type="date" id="date" required />
+                        <label for="d">Date of visit:</label>
+                    </div>
+                    <div class="input-field">
+                        <input type="text" id="tel" required />
+                        <label for="tel">Purpose of Appointment:</label>
+                    </div>
+                    <div class="input-field">
+                        <input type="tel" id="phone" required />
+                        <label for="tel">Phone Number:</label>
+                    </div>
+                    <button>Submit</button>
+                </form>
+                
+            </div>
         </div>
-        <div class="input-field">
-            <input type="text" id="Gender" required />
-            <label for="Gender">Gender:</label>
-        </div>
-        <div class="input-date">
-            <input type="date" id="date" required />
-            <label for="d">Date of visit:</label>
-        </div>
-        <div className="div-visit">
-            <label htmlFor="visit">Purposr of Visit:</label><br />
-            <textarea name="visit" id="visit" cols="5" rows="5" placeholder='Enter the purpose of visit'></textarea>
-        </div>
-        <div class="input-field">
-            <input type="text" id="tel" required />
-            <label for="tel">Phone Number:</label>
-        </div>
-      </form>
-    </div>
-  )
+    )
 }
 
 export default BookAppointmentForm
